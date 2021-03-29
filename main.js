b2c5bd5b3e50d8ea5bc7a823b082362f6bf8080c
Evgeniya Malyushkina <e.malyushkina@mail.ru>
2021-03-29 17:13:43 +0800

    Переделала даты

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const carbone = require("carbone");
 
 carbone.set({
   lang: "ru",
+  timezone: "Europe/Samara",
 });
 
 var data = {
@@ -12,22 +13,22 @@ var data = {
     patronymic: "Петровна",
     passportNumber: "111111111",
     passportIssued: "Ленинским РУВД",
-    passportDate: 20200111,
+    passportDate: "2020-01-11",
     adress: "г. Северомуйск, ул. Потерянных челюскинцев, 25 кв. 5 ",
     inn: 54654654654,
     bik: 654654,
     paymentAccount: 65436546546,
     correspondentAccount: 564654,
-    selfEmployedDate: 20210320,
+    selfEmployedDate: "2021-03-20",
     ifnsName: "ИФНС №3",
     tel: 56465465,
     eMail: "555555555@aa.ru",
   },
   contract: {
     number: 5,
-    contractDate: 20210115,
-    firstDate: 20210325,
-    lastDate: 20210425,
+    contractDate: "2021-01-15",
+    firstDate: "2021-03-25",
+    lastDate: "2021-04-25",
     software: "ПО Договор",
     requirements: [
       { requirement: "требование1" },
@@ -45,10 +46,14 @@ var data = {
   },
 };
 
-carbone.render("devapplications/template/contract_onetime.odt", data, function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+carbone.render(
+  "devapplications/template/contract_onetime.odt",
+  data,
+  function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
 
-  fs.writeFileSync("result.odt", result);
-});
+    fs.writeFileSync("result.odt", result);
+  }
+);
